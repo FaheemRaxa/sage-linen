81cf909e6ffe4808fa74f8f0df0e912fad6b2844
Faheem Raza <121765781+FaheemRaxa@users.noreply.github.com>
2026-08-24 21:02:34 +0500

    Enqueue homepage navigation script and register menu

--- wp-content/themes/sage-linen-child/functions.php
+++ wp-content/themes/sage-linen-child/functions.php
@@ -14,6 +14,14 @@ add_action( 'wp_enqueue_scripts', function () {
     );
 }, 20 );
 
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_script( 'sage-linen-homepage', get_stylesheet_directory_uri() . '/assets/js/homepage.js', array(), '1.0.0', true );
+}, 21 );
+
+add_action( 'after_setup_theme', function () {
+    register_nav_menus( array( 'primary' => __( 'Primary navigation', 'sage-linen' ) ) );
+} );
+
 add_action( 'after_setup_theme', function () {
     add_theme_support( 'custom-logo', array(
         'height'      => 120,
