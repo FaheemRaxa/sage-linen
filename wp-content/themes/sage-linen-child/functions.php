5adaa594c5a824837428d5e87ea77a39a0bfc7ca
Faheem Raza <121765781+FaheemRaxa@users.noreply.github.com>
2026-08-24 20:45:11 +0500

    Add child theme asset loading

---
+++ wp-content/themes/sage-linen-child/functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Sage Linen child theme bootstrap.
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'sage-linen-homepage',
+        get_stylesheet_directory_uri() . '/assets/css/homepage.css',
+        array( 'flatsome-main' ),
+        '1.0.0'
+    );
+}, 20 );
+
+add_action( 'after_setup_theme', function () {
+    add_theme_support( 'custom-logo', array(
+        'height'      => 120,
+        'width'       => 360,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ) );
+} );
